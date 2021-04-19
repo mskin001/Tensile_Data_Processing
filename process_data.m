028375a3c9caf60a3c531ffa186d0acca59cc1af
Miles Skinner <skinner.miles@gmail.com>
2021-04-19 17:11:36 -0600

    Update process_data.m

--- process_data.m
+++ process_data.m
@@ -18,20 +18,37 @@ sg_data = csvread(sg_file, 10, 0); %starts at A11
 
 param_mat = cell2mat(exp_list(6:13));
 instron_param = param_mat(instron_row,:);
-sample_param = param_mat(sg_row,:); % param order is the column names in exp_list starting at row 6
-area = pi * (sample_param(2)^2 - sample_param(1)^2) / 4; % sample cross section area
+sg_param = param_mat(sg_row,:); % param order is the column names in exp_list starting at row 6
+area = pi * (sg_param(2)^2 - sg_param(1)^2) / 4; % sample cross section area
 
 %% ------------------------------------------------------------------------
 %  ---- Average data over each second -------------------------------------
 %  ------------------------------------------------------------------------
-in_temp = reshape(instron_data(:,3),instron_param(end));
-in_avg_load = mean(in_temp);
+test = mod(length(sg_data(:,2)),sg_param(end));
+if ~test == 0
+  sg_data(1:test,:) = [];
+end
+sg_temp = reshape(sg_data(:,2),[sg_param(end),length(sg_data(:,3))/sg_param(end)]);
+sg_avg_mv = mean(sg_temp)';
+exp_start_time = find(abs(sg_avg_mv(1:50)) <=  mean(abs(sg_avg_mv(1:3))));
+sg_avg_mv(1:exp_start_time(end)) = [];
+sg_time = sg_data(exp_start_time(end):sg_param(end):end,1);
+sg_time(1:exp_start_time(end)) = [];
+sg_time = sg_time - sg_time(1);
 
-sg_temp = reshape(sg_data(:,2),sg_param(end));
-sg_avg_load = mean(sg_temp);
+test = mod(length(instron_data(:,3)),instron_param(end));
+if ~test == 0
+  instron_data(1:test,:) = [];
+end
+in_temp = reshape(instron_data(:,3),[instron_param(end),...
+  length(instron_data(:,3))/instron_param(end)]);
+in_avg_load = mean(in_temp)';
+in_time = instron_data(1:instron_param(end):end,1);
+in_time = in_time - in_time(1);
 
-instron_stress = instron_data(:,3) / area;
+in_stress = in_avg_load / area; % [MPa]
 
-V_r = (sg_data(:,2) - mean(sg_data(1:100))) / sample_param(6);
+V_r = (sg_avg_mv - mean(sg_avg_mv(1:3))) / sg_param(6);
 
-sg_strain = (-4 .* V_r) ./ (sample_param(4).*(1 + 2.*V_r)); % Quarter bridge. Assumes lead resistance of the wires is 0
+sg_strain = (-4 .* V_r) ./ (sg_param(4).*(1 + 2.*V_r)); % Quarter bridge. Assumes lead resistance of the wires is 0
+
