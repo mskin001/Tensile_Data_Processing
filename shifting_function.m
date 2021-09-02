ace8b27fb7509e9271849000afcb79827833e785
Miles Skinner <skinner.miles@gmail.com>
2021-09-01 22:40:28 -0600

    create shifting function

--- shifting_function.m
+++ shifting_function.m
@@ -1,10 +1,13 @@
+% Shifting algorithm is from "The closed form t-T-P shifting (CFS)
+% algorithm" by M. Gergesova, B. Zupancic, I. Saprunov, and I. Emri
+
 temps = [40, 50, 60, 70]; % temp in degC
 tref = temps(1);
 
-t1 = load('forty_deg.csv');
-t2 = load('fifty_deg.csv');
-t3 = load('sixty_deg.csv');
-t4 = load('seventy_deg.csv');
+t1 = load('forty.csv');
+t2 = load('fifty.csv');
+t3 = load('sixty.csv');
+t4 = load('seventy.csv');
 
 
 raw_times(:,1) = t1(:,1);
@@ -17,26 +20,58 @@ raw_comp(:,2) = t2(:,2);
 raw_comp(:,3) = t3(:,2);
 raw_comp(:,4) = t4(:,2);
 
+figure(1)
+for k = 1:length(temps)
+    hold on
+    plot(raw_times(:,k), raw_comp(:,k))
+end
+xlabel('log(t) [min]')
+ylabel('Compliance [1/GPa]')
 
-times = log(raw_times);
-comp = log(raw_comp);
-
-% find overlab region
+times = raw_times;
+comp = log10(raw_comp);
 ref_time = times(:,1);
 ref_comp = comp(:,1);
 
-next_time = times(:,2);
-next_comp = comp(:,2);
+figure(2)
+for k = 1:length(temps) -1
+    % find overlab region
+    next_time = times(:,k+1);
+    next_comp = comp(:,k+1);
 
-[~, ol_start] = min(next_comp);
-[~, ol_end] = max(ref_comp);
-u_ind = find(next_comp(:,1) >= next_comp(ol_start) & next_comp(:,1) <= ref_comp(ol_end));
-l_ind = find(ref_comp(:,1) >= next_comp(ol_start) & ref_comp(:,1) <= ref_comp(ol_end));
+    [~, ol_start] = min(next_comp);
+    [~, ol_end] = max(ref_comp);
+    u_ind = find(next_comp(:,1) >= next_comp(ol_start) & next_comp(:,1) <= ref_comp(ol_end));
+    l_ind = find(ref_comp(:,1) >= next_comp(ol_start) & ref_comp(:,1) <= ref_comp(ol_end));
 
-U = [next_time(u_ind), next_comp(u_ind)];
-L = [ref_time(l_ind), ref_comp(l_ind)];
+    U = [next_time(u_ind), next_comp(u_ind)];
+    L = [ref_time(l_ind), ref_comp(l_ind)];
 
-for k = 1:length(U) - 1
+    % A is an intermediate variable representing the area within the overlaping
+    % region of the two compliance curves
+    A = sum((0.5 * (L(2:end,1) + L(1:end-1,1))) .* (L(2:end,2) - L(1:end-1,2)));
+
+    % b is an intermediate variable
+    b = sum((0.5 * (U(2:end,1) + U(1:end-1,1))) .* (U(2:end,2) - U(1:end-1,2)));
+
+    log_shift_factor = (A - b) / (L(end,2) - U(1,2));
+    shift_factor = 10^(log_shift_factor);
+    shifted_time = next_time + log_shift_factor;
+    ref_time = [ref_time; shifted_time];
+    ref_comp = [ref_comp; next_comp];
     
+    hold on
+    loglog(ref_time, ref_comp, '-o', 'LineWidth', 1.5)
+    loglog(next_time, next_comp, '-*', 'LineWidth', 1.5)
+    loglog(shifted_time, next_comp, '-+', 'LineWidth', 1.5)
+end
+figure(2)
+xlabel('log(t) [min]')
+ylabel('log(D) [1/GPa]')
 
+master_comp = 10.^ref_comp;
+figure(3)
+plot(ref_time,master_comp, 'k^', 'LineWidth', 1.5)
+xlabel('log(t) [min]')
+ylabel('Compliance [1/GPa]')
 
