2aeed24e38c4b72ccf09b83d1be1cadef411a07f
Miles Skinner <skinner.miles@gmail.com>
2022-03-02 17:29:41 -0700

    daily work

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ addpath(['H:\My Drive\FESS Student Projects\Miles Skinner\Experimental Data\'...
 % addpath(['H:\My Drive\FESS Student Projects\Miles Skinner\Experimental Data'...
 %     '\Tensile Viscoelastic'])
 
-exp_name = {'cte_exp'};
+exp_name = {'GF11-01'};
 avg_res = true;
 
 legText = {'SG1', 'SG2', 'SG3'};
