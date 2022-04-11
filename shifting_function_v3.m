8299765c098f6a327a362f2a0590bf0eebbb4b1f
Miles Skinner <skinner.miles@gmail.com>
2022-04-10 20:28:19 -0600

    Update shifting_function_v3.m

--- shifting_function_v3.m
+++ shifting_function_v3.m
@@ -7,29 +7,31 @@ comp = comp.avg_comp;
 exp_time = load('avg_time');
 exp_time = exp_time.avg_time;
 temps = [35, 45, 60]; % temp in degC
-beg_lin_resp = [3, 3, 50];
+begin_point = [3, 3, 3];
+shift_modifier = [0,0.62];
 tref = temps(1);
 % 
-figure()
-marker = ['o+^d'];
-for k = 1:length(temps)
-    hold on
-    plot(exp_time{k}, comp{k}, marker(k),'Linewidth', 1.5)
-end
-xlabel('log_{10}(t) [min]')
-ylabel('Compliance [1/GPa]')
-set(gca, 'Fontsize', 12)
+% figure()
+% marker = ['o+^d'];
+% for k = 1:length(temps)
+%     hold on
+%     plot(exp_time{k}, comp{k}, marker(k),'Linewidth', 1.5)
+% end
+% xlabel('log_{10}(t) [min]')
+% ylabel('Compliance [1/GPa]')
+% set(gca, 'Fontsize', 12)
 
 % times = raw_times;
 % comp = log10(raw_comp);
-ref_time = log10(exp_time{1}(beg_lin_resp(1):end))';
-ref_comp = log10(comp{1}(beg_lin_resp(1):end))';
-best_fit_exten = 100000;
+ref_time = log10(exp_time{1}(begin_point(1):end))';
+ref_comp = log10(comp{1}(begin_point(1):end))';
+non_lin = [NaN;NaN];
+best_fit_exten = 10000;
 % figure(), hold on, grid on
 for k = 1:length(temps) -1
 % find overlab region
-    next_time = log10(exp_time{k+1}(beg_lin_resp(k+1):end))';
-    next_comp = log10(comp{k+1}(beg_lin_resp(k+1):end))';
+    next_time = log10(exp_time{k+1}(begin_point(k+1):end))';
+    next_comp = log10(comp{k+1}(begin_point(k+1):end))';
 %     hold on
 %     plot(ref_time,ref_comp)
 %     plot(next_time, next_comp)
@@ -80,23 +82,24 @@ for k = 1:length(temps) -1
         b = sum((0.5 * (U(2:end,1) + U(1:end-1,1))) .* (U(2:end,2) - U(1:end-1,2)));
         
         try
-          log_shift_factor = (A - b) / (L(end,2) - U(1,2));
+          log_shift_factor = (A - b) / (L(end,2) - U(1,2)) - shift_modifier(k);
         catch
           log_shift_factor = NaN;
         end
         
-        best_fit_exten = best_fit_exten*100
+        best_fit_exten = best_fit_exten*100;
     end
         
     shift_factor = 10^(log_shift_factor);
     shifted_time = next_time + log_shift_factor;
     ref_time = [ref_time; shifted_time];
     ref_comp = [ref_comp; next_comp];
+    non_lin = [non_lin(1,:), shifted_time(1:25)'; non_lin(2,:), next_comp(1:25)'];
     
-    figure(), hold on
-    loglog(ref_time, ref_comp, '-o', 'LineWidth', 1.5)
-    loglog(next_time, next_comp, '-*', 'LineWidth', 1.5)
-    loglog(shifted_time, next_comp, '-+', 'LineWidth', 1.5)
+%     figure(), hold on
+%     loglog(ref_time, ref_comp, '-o', 'LineWidth', 1.5)
+%     loglog(next_time, next_comp, '-*', 'LineWidth', 1.5)
+%     loglog(shifted_time, next_comp, '-+', 'LineWidth', 1.5)
 end
 xlabel('log(t) [min]')
 ylabel('log(D) [1/GPa]')
@@ -109,17 +112,18 @@ fit_comp = fitresult.a .* fit_time.^(fitresult.b) + fitresult.c;
 
 figure()
 hold on
-plot(ref_time,mc, 'k^', 'LineWidth', 1.5)
+plot(ref_time,mc, 'k^', 'MarkerFaceColor', 'k', 'LineWidth', 1.5)
+plot(non_lin(1,:),10.^non_lin(2,:), 'b^', 'LineWidth', 1.5)
 marker = ['o+^d'];
 for k = 1:length(temps)
     hold on
     plot(log10(exp_time{k}), comp{k}, marker(k),'Linewidth', 1.5)
 end
-plot(fit_time,10.^fit_comp, '-', 'Linewidth', 1.5)
+plot(fit_time, 10.^fit_comp, '-', 'Color', [0.6350 0.0780 0.1840], 'Linewidth', 1.5)
 grid on
 set(gca, 'Fontsize', 12)
 xlabel('log_{10}(t) [sec]')
 ylabel('Compliance [1/Pa]')
-legend({'Shifted Curves', '30^oC', '45^oC', '60^oC', 'Master Curve'},...
+legend({'Shifted Curves', 'Non-linear Creep', '30^oC', '45^oC', '60^oC', 'Master Curve'},...
     'Location', 'southeast')
 
